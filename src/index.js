e1a27056322d8a3e15b7f3fcb3d2d96549a37784
lisa <elizabet.libet@gmail.com>
2021-10-25 17:13:29 +0500

    add redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/app.scss';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./scss/app.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+
+function counterReducer(state = 0, action) {
+   if (action.type === "INCREMENT"){
+      return state + 1;
+   }
+   if (action.type === "DECREMENT"){
+      return state - 1;
+   }
+  return state;
+}
+
+function rootReducer(state, action) {
+   counterReducer(state, action);
+   counterReducer2(state, action);
+   counterReducer3(state, action);
+}
+let store = createStore(rootReducer);
+ 
+store.subscribe(() => console.log("хранилище изменилось", store.getState()));
+
+console.log(store.getState());
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
 
 ReactDOM.render(
-     <BrowserRouter>
-        <App/>
-     </BrowserRouter>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
